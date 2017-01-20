149a19f18e1ad889326a26a7387df3ea8bf6f990
Rob Connolly <rob@webworxshop.com>
2017-01-21 12:57:05 +1300

    Update to latest Mycroft version. Implement basic SSL support.
    Get working and test with latest Mycroft version. Implemented basic
    (insecure) SSL support for HASS API.

--- __init__.py
+++ __init__.py
@@ -10,22 +10,29 @@ __author__ = 'robconnolly'
 
 class HomeAssistantClient(object):
 
-    def __init__(self, host, password, port=8123):
-        self.url = "http://%s:%d" % (host, port)
+    def __init__(self, host, password, port=8123, ssl=False):
+        self.ssl = ssl
+        if self.ssl:
+            self.url = "https://%s:%d" % (host, port)
+        else:
+            self.url = "http://%s:%d" % (host, port)
         self.headers = {
             'x-ha-access': password,
             'Content-Type': 'application/json'
         }
 
     def execute_service(self, domain, service, data):
-        post("%s/api/services/%s/%s" % (self.url, domain, service), headers=self.headers, data=json.dumps(data))
+        if self.ssl:
+            post("%s/api/services/%s/%s" % (self.url, domain, service), headers=self.headers, data=json.dumps(data), verify=False)
+        else:
+            post("%s/api/services/%s/%s" % (self.url, domain, service), headers=self.headers, data=json.dumps(data))
 
 # TODO - Localization
 class HomeAssistantSkill(MycroftSkill):
     def __init__(self):
         super(HomeAssistantSkill, self).__init__(name="HomeAssistantSkill")
         self.ha = HomeAssistantClient(self.config.get('host'),
-            self.config.get('password'))
+            self.config.get('password'), ssl=self.config.get('ssl', False))
 
     def initialize(self):
         self.load_vocab_files(join(dirname(__file__), 'vocab', 'en-us'))
@@ -41,8 +48,8 @@ class HomeAssistantSkill(MycroftSkill):
             self.register_regex(prefix + ' ' + suffix_regex)
 
     def handle_intent(self, message):
-        entity = message.metadata.get("Entity")
-        action = message.metadata.get("Action")
+        entity = message.data["Entity"]
+        action = message.data["Action"]
 
         # TODO: we should use the HA api to search for a matching entity
         ha_entity = {"entity_id": "light." + entity.replace(" ", "_")}
